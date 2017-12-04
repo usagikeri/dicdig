eca33153724ca621bacd6e641b2e07ee27bb2c6d
Usagikeri <a.k.a.usagikeri@gmail.com>
2017-12-04 14:48:00 +0900

    bug fix

--- dicdig.py
+++ dicdig.py
@@ -1,25 +1,39 @@
-
 def dicdig(dic, word):
-    for key in list(filter(lambda x: (x == word) or \
-                                     (type(dic[x]) == dict) or \
-                                     (type(dic[x]) == list), dic.keys())):
-        if key == word:
-            return dic[key]
-        else:
-            val = dic[key]
-            if type(val) == dict:
-                return dicdig(val, word)
-            elif type(val) == list:
-                for d in val:
-                    if type(d) == dict:
-                        if dicdig(d, word) != None:
-                            return dicdig(d, word)
-                    else:
-                        raise ValueError("Error: unexpected struct of dictionary.")
-                        break
+    if type(dic) == dict:
+        for key in list(filter(lambda x: (x == word) or
+                                         (type(dic[x]) == dict) or
+                                         (type(dic[x]) == list), dic.keys())):
+            if key == word:
+                return dic[key]
+            else:
+                val = dic[key]
+                if type(val) == dict:
+                    return dicdig(val, word)
+                elif type(val) == list:
+                    for i in list(filter(lambda x: (type(x) == dict) or
+                                                   (type(x) == list), val)):
+                        if type(i) == dict:
+                            if dicdig(i, word) is not None:
+                                return dicdig(i, word)
+                        else:
+                            if dicdig(i, word) is not None:
+                                return dicdig(i, word)
+    elif type(dic) == list:
+        for j in list(filter(lambda x: (type(x) == dict) or
+                                       (type(x) == list), dic)):
+            if type(j) == dict:
+                if dicdig(j, word) is not None:
+                    return dicdig(j, word)
+            elif type(j) == list:
+                for k in list(filter(lambda x: (type(x) == dict) or
+                                               (type(x) == list), j)):
+                    if dicdig(k, word) is not None:
+                        return dicdig(k, word)
 
 
 if __name__ == "__main__":
-    d = {"a": 1, "b": [{"c": 3}, {"d": [{"e": "Goal!!", "f": 4}]}]}
+    d = {"a": 1, "b": [{"c": 3},
+         {"d": [[1], [{"g": 5},
+          {"e": "Goal!!", "f": 4}]]}]}
     x = dicdig(d, "e")
     print(x)
